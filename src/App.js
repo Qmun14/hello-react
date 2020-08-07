5d64f9e87710c53c3cc0cc2f72709824e0c0f672
Qmun14 <mamunramdhan@gmail.com>
2020-08-07 17:33:40 +0700

    Merubah caption anchor di body App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Dengan Asik dan Santai
         </a>
       </header>
     </div>
